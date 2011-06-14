bc9c45a6724c4ccbef8389adab59d7f7c81f7e83
isacsandim@gmail.com <isacsandim@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-06-14 12:10:28 +0000

    commit inicial

---
+++ CinePointer/src/br/com/cinepoiter/Filme.java
@@ -0,0 +1,26 @@
+package br.com.cinepoiter;
+
+public class Filme {
+
+    private String nome;
+    private int banner;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getBanner() {
+        return banner;
+    }
+
+    public void setBanner(int banner) {
+        this.banner = banner;
+    }
+
+}
+
+
