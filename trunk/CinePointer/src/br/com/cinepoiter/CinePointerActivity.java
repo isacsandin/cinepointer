8c8a17f949aa5bac23b213b25969e65822a958f7
isacsandim@gmail.com <isacsandim@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-06-14 14:54:51 +0000

    agora com a lista funcionando!!! :)

--- trunk/CinePointer/src/br/com/cinepoiter/CinePointerActivity.java
+++ trunk/CinePointer/src/br/com/cinepoiter/CinePointerActivity.java
@@ -1,70 +1,68 @@
 package br.com.cinepoiter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.R.array;
 import android.app.Activity;
-import android.app.ListActivity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Spinner;
 
 public class CinePointerActivity extends Activity {
     /** Called when the activity is first created. */
-
+	
+	AlertDialog.Builder dialogo; 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dialogo = new AlertDialog.Builder(CinePointerActivity.this);
         setContentView(R.layout.inicial);
 	
         
         String [] array_spinner =new String[5];
-        array_spinner[0]="Cidade 1";
-        array_spinner[1]="Cidade 2";
-        array_spinner[2]="Cidade 3";
-        array_spinner[3]="Cidade 4";
-        array_spinner[4]="Cidade 5";
+        array_spinner[0]="Todas";
+        array_spinner[1]="Cidade 1";
+        array_spinner[2]="Cidade 2";
+        array_spinner[3]="Cidade 3";
+        array_spinner[4]="Cidade 4";
         
         Spinner s = (Spinner) findViewById(R.id.spinner1);
       
         setSpinnerOptions(array_spinner,s);
  
 		array_spinner =new String[5];
-        array_spinner[0]="Gênero 1";
-        array_spinner[1]="Gênero 2";
-        array_spinner[2]="Gênero 3";
-        array_spinner[3]="Gênero 4";
-        array_spinner[4]="Gênero 5";
+		array_spinner[0]="Todos";
+        array_spinner[1]="Gênero 1";
+        array_spinner[2]="Gênero 2";
+        array_spinner[3]="Gênero 3";
+        array_spinner[4]="Gênero 4";
         
         s = (Spinner) findViewById(R.id.spinner2);
         
         setSpinnerOptions(array_spinner,s);
 
-		array_spinner =new String[3];
-        array_spinner[0]="Manhã";
-        array_spinner[1]="Tarde";
-        array_spinner[2]="Noite";
+		array_spinner =new String[4];
+		array_spinner[0]="Todos";
+        array_spinner[1]="Manhã";
+        array_spinner[2]="Tarde";
+        array_spinner[3]="Noite";
         
         s = (Spinner) findViewById(R.id.spinner3);
         
         setSpinnerOptions(array_spinner,s);
         
         Button b = (Button) findViewById(R.id.botaoPesquisar);
-        
+                
         b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				imprimirMensagem("teste","estou aqui...");
 				Intent i = new Intent(CinePointerActivity.this,SoftwarePassionView.class);
+				imprimirMensagem("teste",i.toURI());
 				startActivity(i);
 			}
 		});
-       
-
- 
     }
     
 	private void setSpinnerOptions(String [] array_spinner, Spinner s) {
@@ -72,6 +70,13 @@ public class CinePointerActivity extends Activity {
         android.R.layout.simple_spinner_item, array_spinner);
         s.setAdapter(adapter);
 	}
+	
+	public void imprimirMensagem(String title, String msg){
+		dialogo.setTitle(title);
+		dialogo.setMessage(msg);
+		dialogo.setNeutralButton("OK", null);
+		dialogo.show();
+		}
 	
 	
 }
