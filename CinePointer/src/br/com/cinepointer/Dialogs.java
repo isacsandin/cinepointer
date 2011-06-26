4222a0b0820af91c1d6f40f7d9f2416ed9fac6b4
isacsandim@gmail.com <isacsandim@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-06-26 22:30:25 +0000

    finalizando adicao do suporte ao google maps

---
+++ CinePointer/src/br/com/cinepointer/Dialogs.java
@@ -0,0 +1,16 @@
+package br.com.cinepointer;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class Dialogs {
+	
+	public static void imprimirMensagem(Context con,String title, String msg){
+		AlertDialog.Builder dialogo; 
+        dialogo = new AlertDialog.Builder(con);
+		dialogo.setTitle(title);
+		dialogo.setMessage(msg);
+		dialogo.setNeutralButton("OK", null);
+		dialogo.show();
+		}
+}
