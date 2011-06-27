12a437bc69b5a0972e426e75e2eaaff02c41309c
isacsandim@gmail.com <isacsandim@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-06-27 15:34:20 +0000

    adicionando maior suporte ao maps,como tracado de rotas e verificação de distâncias

--- trunk/CinePointer/src/br/com/cinepointer/ui/CinePointerActivity.java
+++ trunk/CinePointer/src/br/com/cinepointer/ui/CinePointerActivity.java
@@ -12,49 +12,49 @@ import br.com.cinepointer.R;
 
 public class CinePointerActivity extends Activity {
 
-		
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.inicial);
-	
-        
-        String [] array_spinner = getResources().getStringArray(R.array.arrayCidades);
-        
-        Spinner s = (Spinner) findViewById(R.id.spinner1);
-      
-        setSpinnerOptions(array_spinner,s);
- 
-		array_spinner = getResources().getStringArray(R.array.arrayGeneros);
-        
-        s = (Spinner) findViewById(R.id.spinner2);
-        
-        setSpinnerOptions(array_spinner,s);
 
-        array_spinner = getResources().getStringArray(R.array.arrayHorarios);
-        
-        s = (Spinner) findViewById(R.id.spinner3);
-        
-        setSpinnerOptions(array_spinner,s);
-        
-        Button b = (Button) findViewById(R.id.botaoPesquisar);
-                
-        b.setOnClickListener(new View.OnClickListener() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.inicial);
+
+
+		String [] array_spinner = getResources().getStringArray(R.array.arrayCidades);
+
+		Spinner s = (Spinner) findViewById(R.id.spinner1);
+
+		setSpinnerOptions(array_spinner,s);
+
+		array_spinner = getResources().getStringArray(R.array.arrayGeneros);
+
+		s = (Spinner) findViewById(R.id.spinner2);
+
+		setSpinnerOptions(array_spinner,s);
+
+		array_spinner = getResources().getStringArray(R.array.arrayHorarios);
+
+		s = (Spinner) findViewById(R.id.spinner3);
+
+		setSpinnerOptions(array_spinner,s);
+
+		Button b = (Button) findViewById(R.id.botaoPesquisar);
+
+		b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(CinePointerActivity.this,ListFilmView.class);
 				startActivity(i);
 			}
 		});
-    }
-    
-	private void setSpinnerOptions(String [] array_spinner, Spinner s) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        android.R.layout.simple_spinner_item, array_spinner);
-        s.setAdapter(adapter);
 	}
-	
 
-	
-	
+	private void setSpinnerOptions(String [] array_spinner, Spinner s) {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, array_spinner);
+		s.setAdapter(adapter);
+	}
+
+
+
+
 }
