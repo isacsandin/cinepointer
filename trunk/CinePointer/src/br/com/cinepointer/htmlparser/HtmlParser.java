3c46896e6717499a9fadcc3471cd031f3e3d8921
feliperviegas@gmail.com <feliperviegas@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-07-03 19:26:31 +0000

    Edição parserHtml

--- trunk/CinePointer/src/br/com/cinepointer/htmlparser/HtmlParser.java
+++ trunk/CinePointer/src/br/com/cinepointer/htmlparser/HtmlParser.java
@@ -1,6 +1,10 @@
 package br.com.cinepointer.htmlparser;
 
+import br.com.cinepointer.datatypes.*;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,42 +19,73 @@ public class HtmlParser {
 	public static void parse() throws IOException{
 
 		//String url = "http://www.cinemaki.com.br/shows";
-		//String url ="http://www.cinemaki.com.br/cinemas/tag/95";
+		String url ="http://www.cinemaki.com.br/cinemas/tag/95";
 		//String url ="http://www.cinemaki.com.br/Cinemark-Diamond-Mall/t/2806";
 		//String url = "http://www.cinemaki.com.br/Anima%C3%A7%C3%A3o-/shows/tag/54";
-		String url = "http://www.cinemaki.com.br/Carros-2/p/2339";
+		//String url = "http://www.cinemaki.com.br/Carros-2/p/2339";
 
 		while(url != null ){
 
 			//url = processCinemPageDetails(url);
-			//url = processCinemPage(url);
+			ArrayList<Cinema> ListaCinema = processCinemPage(url);
 			//url = processFilmPage(url);
-			url = processFilmDetailsPage(url);
+			//ArrayList<Filme> ListaFilme  = processFilmDetailsPage(url);
+			url =null;
 		}
 
 	}
-	private static String processFilmDetailsPage(String url) throws IOException {
-
+	
+	//Detalhes Filme
+	private static ArrayList<Filme> processFilmDetailsPage(String url) {
+        
+		Filme filme = new Filme();
+		ArrayList<Filme> listFilme = new ArrayList<Filme>();
+		HashMap<String, String> hash = new HashMap<String, String>();
 		url +="?version=desktop";
 
-		Document doc = Jsoup.connect(url).get();
-		
+		Document doc;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+		Elements imgs = doc.select("img[class~=new_4col]");
+			filme.setBanner(imgs.first().attr("src"));
+
 		Elements links = doc.select("[class~=new_7col fl m10l]");
 		for (Element link : links) {
-			Log.d("HTMLPARSER::",link.select("table.fl").attr("summary"));
+			filme.setNome(link.select("table.fl").attr("summary"));
+			Log.d("HTMLPARSER::",filme.getNome());
 			for (Element element : link.select("table.fl").select("tr")) {
-				Log.d("HTMLPARSER::",element.select("td.first").text()+": ");
-				Log.d("HTMLPARSER::",element.select("td").text());
+				 String aux ="";			
+
+			      aux = element.select("td.first").text() + ": "+ element.select("td:not([class~=first])").text();
+				  String[] str = aux.split(": ");
+				  hash.put(str[0].trim(), str[1].trim());
 			}
+			
+			filme.setGenero(hash.get("Gênero"));
+			Log.d("HTMLPARSER::",filme.getGenero());
+			filme.setDuracao(hash.get("Duração"));
+			Log.d("HTMLPARSER::",filme.getDuracao());
+			filme.setDiretor(hash.get("Diretor"));
+			Log.d("HTMLPARSER::",filme.getDiretor());
+			filme.setAtoresPrincipais(hash.get("Atores principais"));
+			Log.d("HTMLPARSER::",filme.getAtoresPrincipais());
+			
 			Element sinopse = doc.select("[class~=synopsis dn fcl]").first();
-			Log.d("HTMLPARSER::","sinopse:\n"+sinopse.text());
+			filme.setSinopse(sinopse.text());
+			Log.d("HTMLPARSER::",filme.getSinopse());
 			Log.d("HTMLPARSER::","==========");
+			listFilme.add(filme);
 		}
 
-		return null;
+		return listFilme;
 	}
 	
 	
+	//
 	private static String processCinemPageDetails(String url) throws IOException {
 
 		url +="?version=desktop";
@@ -74,25 +109,48 @@ public class HtmlParser {
 	}
 	
 
-	private static String processCinemPage(String url) throws IOException {
-
+	//Informações de Cinemas de uma cidade
+	//String url ="http://www.cinemaki.com.br/cinemas/tag/95";
+	private static ArrayList<Cinema> processCinemPage(String urlEntrada){
+		
+		ArrayList<Cinema> listCinema = new ArrayList<Cinema>();
+		Cinema cinema = new Cinema();
+		HashMap<String, String> hash = new HashMap<String, String>();
+		
+		String url = urlEntrada;
 		url +="?version=desktop";
 
-		Document doc = Jsoup.connect(url).get();
+		Document doc;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			return null;
+		}
 		
 		Elements links = doc.select("[class~=p10 theater_row]");
 		for (Element link : links) {
-			Log.d("HTMLPARSER::","NOME: "+link.select("h4").first().text());
-			Log.d("HTMLPARSER::","LINK: "+link.select("h4 > a").first().attr("abs:href"));
+			cinema.setNome(link.select("h4").first().text());
+			Log.d("HTMLPARSER::",cinema.getNome());
+			cinema.setLink(link.select("h4 > a").first().attr("abs:href"));
+			Log.d("HTMLPARSER::",cinema.getLink());
 			Log.d("HTMLPARSER::","DETALHES:");
 			for (Element element : link.select("[class~=m5b]")) {
-				Log.d("HTMLPARSER::",element.text());
+				String[] str = element.text().split(": ");
+				hash.put(str[0].trim(), str[1].trim());
 			}
+			cinema.setEndereco(hash.get("Endereço"));
+			Log.d("HTMLPARSER::",cinema.getEndereco());
+			cinema.setTelefone(hash.get("Telefone"));
+			Log.d("HTMLPARSER::",cinema.getTelefone());
+			cinema.setQuantidade(hash.get("Quantidade de salas"));
+			Log.d("HTMLPARSER::",cinema.getQuantidade());
 			Log.d("HTMLPARSER::","==========");
+			listCinema.add(cinema);
 		}
 
-		return null;
+		return listCinema;
 	}
+
 
 
 
