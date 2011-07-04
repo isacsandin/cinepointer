82be14778ddd755c0f6b684dd61e2ea10c0c42be
isacsandim@gmail.com <isacsandim@gmail.com@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-07-04 00:55:28 +0000

    mudando rotas de pesquisa a adicionando novos casos de uso

--- trunk/CinePointer/src/br/com/cinepointer/ui/CinePointerActivity.java
+++ trunk/CinePointer/src/br/com/cinepointer/ui/CinePointerActivity.java
@@ -1,20 +1,15 @@
 package br.com.cinepointer.ui;
 
 import java.io.IOException;
-import java.util.GregorianCalendar;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.RadioGroup;
 import br.com.cinepointer.R;
-import br.com.cinepointer.database.CalendarData;
-import br.com.cinepointer.datatypes.Variables;
 import br.com.cinepointer.htmlparser.HtmlParser;
 
 public class CinePointerActivity extends Activity {
@@ -24,70 +19,40 @@ public class CinePointerActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.inicial);
-
-		Spinner s = (Spinner) findViewById(R.id.spinner1);
-		String [] array_spinner = Variables.getInstance().getAllStates();
-		setSpinnerOptions(array_spinner,s);
-		
-		s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-		    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-		    	String estado = (String) parent.getItemAtPosition(pos);
-				String [] array_spinner = Variables.getInstance().getCitiesFrom(Variables.getInstance().getStateCode(estado));
-				Spinner spi = (Spinner) findViewById(R.id.spinner2);
-				if(spi !=null){
-					setSpinnerOptions(array_spinner,spi);
-					spi.setEnabled(true);
-					}
-				else{
-				   Dialogs.imprimirMensagem(parent.getContext(),"ERRO","spinner e nulo");
-				}	
-		    }
-		    public void onNothingSelected(AdapterView<?> parent) {
-		    }
-		});
+		setContentView(R.layout.escolhe_pesquisa);
 
 
-		
-//		String estado = (String) parent.getItemAtPosition(pos);
-//        Dialogs.imprimirMensagem(getApplicationContext(),"debug",estado);
-
-		
-		s = (Spinner) findViewById(R.id.spinner2);
-		s.setEnabled(false);
-		
-		s = (Spinner) findViewById(R.id.spinner3);
-		array_spinner = Variables.getInstance().getAllGenres();
-		setSpinnerOptions(array_spinner,s);
-
-
-//		CalendarData.addToCalendar(this,"Compromisso1",
-//		new GregorianCalendar(2011,6,30,17,30).getTimeInMillis(),
-//		new GregorianCalendar(2011,6,30,19,30).getTimeInMillis()
-//		);
-		
 		try {
 			HtmlParser.parse();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		Button b = (Button) findViewById(R.id.botaoPesquisar);
 
-		
+		Button b = (Button) findViewById(R.id.escolhePesquisaBotaoPesquisar);
+
+
 		b.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent i = new Intent(CinePointerActivity.this,ListFilmView.class);
-				startActivity(i);
+
+				RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+				Intent i;
+				switch(rg.getCheckedRadioButtonId()){
+
+				case R.id.radio0:
+					i = new Intent(CinePointerActivity.this,PesquisaNomeActivity.class);
+					startActivity(i);
+					break;
+				case R.id.radio1:
+					i = new Intent(CinePointerActivity.this,PesquisaLocalizacaoActivity.class);
+					startActivity(i);
+					break;
+				default:
+					Dialogs.imprimirMensagem(getApplicationContext(),"ERRO","FERROU");
+					break;
+				}
 			}
 		});
-	}
-
-	private void setSpinnerOptions(String [] array_spinner, Spinner s) {
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, array_spinner);
-		s.setAdapter(adapter);
 	}
 
 
--- trunk/CinePointer/src/br/com/cinepointer/ui/Dialogs.java
+++ trunk/CinePointer/src/br/com/cinepointer/ui/Dialogs.java
@@ -2,9 +2,12 @@ package br.com.cinepointer.ui;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 public class Dialogs {
+	
+	static String  escolhido = "";
 	
 	public static void imprimirMensagem(Context con,String title, String msg){
 		AlertDialog.Builder dialogo; 
@@ -14,8 +17,26 @@ public class Dialogs {
 		dialogo.setNeutralButton("OK", null);
 		dialogo.show();
 		}
+	
 	public static void imprimirTexto(Context con,String msg){
 		Toast.makeText(con,msg,Toast.LENGTH_LONG).show();
+	}
+	
+	public static void listDialog(final Context con,String title, final String[] items){
+		
+		AlertDialog.Builder dialogo; 
+		dialogo = new AlertDialog.Builder(con);
+		
+        DialogInterface.OnClickListener ocl = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            	escolhido = items[which];
+            }
+        };
+		
+        dialogo.setTitle(title);
+        dialogo.setItems(items,ocl);
+        dialogo.create();
+        dialogo.show();  
 	}
 
 }
