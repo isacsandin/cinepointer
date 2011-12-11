9657687ab8196d34c31da04eb77627fec769c2cb
luizcpo <luizcpo@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-12-11 10:48:34 +0000

    Inclusão dos arquivos conecta.php e getDataTerms.php.

---
+++ trunk/tpBD2/conecta.php
@@ -0,0 +1,19 @@
+<?
+
+/*
+-------------------------------------------------------------
+	Servidor MySQL:     localhost
+	Banco de dados:     bd01
+	Usuario de acesso:  usuario
+	Senha do usuario:   minicurso
+*/
+
+$dbserver  = "localhost";	// Servidor onde está o banco de dados
+$dbname    = "wp_TP_BD";		// Nome do banco de dados MySQL 
+$dbuser    = "usuarioMySQL";		// Uusuariosuario MySQL para conexao 
+$dbpassw   = "naosei";	// Senha do usuario MySQL
+
+$conexao = mysql_connect($dbserver,$dbuser,$dbpassw) or die(mysql_error());
+mysql_select_db($dbname,$conexao) or die(mysql_error());
+
+?>
