9a79dbf327d0ba1fdaca1debaa42b74f79ebe029
luizcpo <luizcpo@fe234a73-e2ed-72c2-209a-5a73890fa869>
2011-12-12 00:13:21 +0000

    Inserção do plugin phpexec. Inclusão do twitter widget no arquivo sidebar.php. Teste de php na página http://localhost/~luizcpo/tpBD2/?p=41.

--- tpBD2/wp-content/themes/blue-with-grey/sidebar.php
+++ tpBD2/wp-content/themes/blue-with-grey/sidebar.php
@@ -50,6 +50,38 @@
 
     </script>
         </div>
+<div>
+    <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'search',
+  search: 'corrupção',
+  interval: 30000,
+  title: 'Tweets em tempo real...',
+  subject: '',
+  width: 298,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#eee',
+      color: '#b30d0d'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#000000',
+      links: '#1985b5'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: true,
+    live: true,
+    behavior: 'default'
+  }
+}).render().start();
+</script>
+</div>
         <?php /* If this is the frontpage  if ( is_home() || is_page() ) { */ ?>
         <!--			<div class="widget">
                                         <h3><?php // _e( 'Meta', 'blue_with_grey' );  ?></h3>
